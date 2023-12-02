5201644890e9a2b856fcf626d16f0aa0dbcbf5d0
Diego <diegopablo012@gmail.com>
2023-12-01 21:10:09 -0600

    Update input class and fix title in controlPanel.html

---
+++ public/js/finance.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    const menuItems = $(".subitem-menu");
+    const contenidoItems = $(".container-item");
+    contenidoItems.hide();
+    $("#suppliers").show();
+    menuItems.click(function(event) {
+        event.preventDefault();
+        const target = $(this).data("target");
+        contenidoItems.hide();
+        $("#" + target).show();
+        menuItems.removeClass("selected-item");
+        $(this).addClass("selected-item");
+    });
+});
