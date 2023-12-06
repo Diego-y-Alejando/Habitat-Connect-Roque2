dced86e7bc251b6fb60920204e79d8cd9736bd79
Diego <diegopablo012@gmail.com>
2023-12-05 18:49:37 -0600

    Refactor apartment details page

--- public/js/apartmentDetails.js
+++ public/js/apartmentDetails.js
@@ -1,14 +1,65 @@
 $(document).ready(function() {
-    const menuItems = $(".subitem-menu");
-    const contenidoItems = $(".container-item");
-    contenidoItems.hide();
+
+    $(".container-item").hide();
     $("#apartment-data").show();
-    menuItems.click(function(event) {
+    $(".subitem-menu").click(function(event) {
         event.preventDefault();
         const target = $(this).data("target");
-        contenidoItems.hide();
+        $(".container-item").hide();
         $("#" + target).show();
-        menuItems.removeClass("selected-item");
+        $(".subitem-menu").removeClass("selected-item");
         $(this).addClass("selected-item");
+    });
+
+    $('#btn-family-edit').click(function(event) {  
+        event.preventDefault();
+
+        if($("#img-save-family").hasClass("hide")){
+            $("#img-edit-family").addClass("hide");
+            $("#img-save-family").removeClass("hide");
+            $("#input-family").removeAttr("disabled");
+        }
+        else{
+            $("#img-save-family").addClass("hide");
+            $("#img-edit-family").removeClass("hide");
+            $("#input-family").attr("disabled", "disabled");
+            //aqui se actualizaria en BD
+        }
+    });
+
+    $("#btn-owner-edit").click(function(event) {    
+        event.preventDefault();
+
+        if($("#img-save-owner").hasClass("hide")){
+            $("#img-edit-owner").addClass("hide");
+            $("#img-save-owner").removeClass("hide");
+            $("#input-owner-name").removeAttr("disabled");
+            $("#input-owner-phone").removeAttr("disabled");
+        }
+        else{
+            $("#img-save-owner").addClass("hide");
+            $("#img-edit-owner").removeClass("hide");
+            $("#input-owner-name").attr("disabled", "disabled");
+            $("#input-owner-phone").attr("disabled", "disabled");
+            //aqui se actualizaria en BD
+        }
+    });
+
+    $("#btn-tenant-edit").click(function(event) {
+        event.preventDefault();
+
+        if($("#img-save-tenant").hasClass("hide")){
+            $("#img-edit-tenant").addClass("hide");
+            $("#img-save-tenant").removeClass("hide");
+            $("#input-tenant-name").removeAttr("disabled");
+            $("#input-tenant-phone").removeAttr("disabled");
+        }
+        else{
+            $("#img-save-tenant").addClass("hide");
+            $("#img-edit-tenant").removeClass("hide");
+            $("#input-tenant-name").attr("disabled", "disabled");
+            $("#input-tenant-phone").attr("disabled", "disabled");
+            //aqui se actualizaria en BD
+        }
     });
 });
