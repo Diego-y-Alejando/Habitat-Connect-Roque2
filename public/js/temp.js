2002e80433a738da067813a05d0ff735ee19e552
Diego <diegopablo012@gmail.com>
2023-12-16 16:01:31 -0600

    Refactor CSS and HTML code

--- public/js/temp.js
+++ public/js/temp.js
@@ -1,81 +1,26 @@
-import {
-    validationEmail,
-    validationPassword,
-    validationDepartament
-}from './validators.js'
-import {
-  BASE_URL,
-  showError,
-  setTokenLocalStorage,
-  getLocalStorageItem,
-  makeRequest,
-  redirectTo
-}from './helpers.js'
-(function($, window, document) {  
-    const logginForm = document.getElementById('loggin-form');
-    const selectDepartament = document.getElementById('departament');
-    const btnLoggin= document.getElementById('btn-loggin');
-    const errorSpan = document.getElementById('loggin-error');
-
-    logginForm.addEventListener('submit', async function(event) {
-        event.preventDefault(); 
-        
-        const departamenturl = selectDepartament.value.substring(0, selectDepartament.value.indexOf("-")).toLowerCase();
-        const body = new URLSearchParams({
-            email:logginForm.email.value,
-            password:logginForm.password.value,
-            departament:selectDepartament.value
-        }).toString();
-        
-        try{
-          const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          }
-          const data = await makeRequest(BASE_URL+departamenturl+'/loggin','POST',body,headers)
-          if (data.ok===false) {
-            
-            showError(errorSpan,data.error);
-          }else{
-            setTokenLocalStorage(departamenturl);
-            chooseWindowLocation(departamenturl);          
-          }
-        } catch (error) {
-          showError(errorSpan,error.message)
-        }
+(function($, window, document) {    
+  const  bookinBtn = $('.booking-btn');
+  const closeWindowBtn =$('.close-window')
+  bookinBtn.each(function() {
+      $(this).on('click', function() {
+        const containerBookingFormAndCalendar=$(this).parent().siblings('.container-booking-form-calendar')
+        containerBookingFormAndCalendar.css({
+          display: 'flex',
+        }).animate({
+              opacity:1
+          }, 400, function() {
+              console.log('Animación completada');
+          });
       });
-
-    btnLoggin.addEventListener('click',(event)=>{
-      try{
-        validationDepartament(selectDepartament.value);
-        validationEmail(logginForm.email.value);
-        validationPassword(logginForm.password.value)
-      } catch (error) {
-        event.preventDefault()
-        showError(errorSpan,error.message)
-      }
-    });
-
-const chooseWindowLocation=(departament)=>{
-  switch (departament) {
-    case 'sales':
-      window.location=BASE_URL+departament+'/patients/?page=1'
-      break;
-    case 'prod':
-      window.location=BASE_URL+departament+'/laboratories/?page=1'
-      break;
-    case 'reception':
-      window.location=BASE_URL+departament+'/subtickets/?page=1'
-      break;
-    case 'med':
-      window.location=BASE_URL+departament+'/patients/?page=1'
-    break;
-    case 'manager':
-      window.location=BASE_URL+departament+'/employees/'
-    break;
-  
-    default:
-      break;
-  }
-}
-
-}(window.jQuery, window, document));
+  });
+  closeWindowBtn.each(function() {
+      $(this).on('click', function() {
+         const closeContainer =$(this).closest('.container-booking-form-calendar');
+         closeContainer.animate({
+              opacity:0
+          }, 400).css({
+              display: 'none',
+          });
+      });
+  });
+}(window.jQuery, window, document)); llegaaa
