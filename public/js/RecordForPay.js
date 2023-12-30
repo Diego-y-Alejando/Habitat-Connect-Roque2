405abac111562450e6713000dc7934c50d5ee414
Diego <diegopablo012@gmail.com>
2023-12-29 19:10:59 -0600

    Update submenu styling and add submenu functionality

---
+++ public/js/RecordForPay.js
@@ -0,0 +1,79 @@
+function loadRecordsForPay() {
+    /* $.ajax({
+        url: "/supplier",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            let suppliers = data.suppliers;
+            let html = "";
+            suppliers.forEach(supplier => {
+                html += `<div class="supplier-name" data-phone="${supplier.phone}">${supplier.name}</div>`;
+            });
+            $(".suppliers-list").html(html);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    }); */
+
+    let recordsForPay = [
+        new RecordForPay("Juan Perez", "12345678", "29-11-23", "Q230.00", true),
+        new RecordForPay("María Rodríguez", "98765432", "29-10-23", "Q2250.00", true),
+        new RecordForPay("Pedro Gómez", "55555555", "29-12-23", "Q1530.00", false),
+    ];
+
+
+    const tableRow = $("<tr>").addClass("table-row");
+    const reocrdSupplier = $("<td>").addClass("record-supplier-name");
+    const recordNumberBill = $("<td>");
+    const recordDate = $("<td>");
+    const recordPrice = $("<td>");
+    const recordState = $("<td>");
+    const recordCheckBox = $("<input>").attr("type", "checkbox");
+
+
+    recordsForPay.forEach(record => {
+        const newRow = tableRow.clone();
+
+        const clonedReocrdSupplier = reocrdSupplier.clone();
+        const clonedRecordNumberBill = recordNumberBill.clone();
+        const clonedRecordDate = recordDate.clone();
+        const clonedRecordPrice = recordPrice.clone();
+        const clonedRecordState = recordState.clone();
+        const clonedRecordCheckBox = recordCheckBox.clone();
+    
+        clonedReocrdSupplier.text(record.supplier);//.addClass("supplier-name");
+        clonedRecordNumberBill.text(record.numberBill);
+        clonedRecordDate.text(record.date);
+        clonedRecordPrice.text(record.amount);
+        
+        if(record.isPayed){
+            //clonedRecordState.text("Pagado");
+            clonedRecordCheckBox.attr("checked", "checked");
+        }else{
+            //clonedRecordState.text("Pendiente");
+            clonedRecordCheckBox.removeAttr("checked");
+        }
+        clonedRecordState.append(clonedRecordCheckBox);
+
+        newRow.append(clonedReocrdSupplier, clonedRecordNumberBill, clonedRecordDate, clonedRecordPrice, clonedRecordState);
+    
+        $("#table-record-for-pay").append(newRow);
+    });
+}
+
+
+
+class RecordForPay{
+    constructor(supplier, numberBill ,date, amount, isPayed){
+        this.supplier = supplier;
+        this.numberBill = numberBill;
+        this.date = date;
+        this.amount = amount;
+        this.isPayed = isPayed;
+    }
+}
+
+export{
+    loadRecordsForPay
+}
--- public/js/finance.js
+++ public/js/finance.js
@@ -1,3 +1,11 @@
+import {
+    loadSuppliers
+} from './Supplier.js';
+import {
+    loadRecordsForPay
+} from './RecordForPay.js';
+
+
 $(document).ready(function() {
 
     loadSuppliers();
@@ -6,14 +14,7 @@ $(document).ready(function() {
 
     $(".container-item").hide();
     $("#suppliers").show();
-    $(".subitem-menu").click(function(event) {
-        event.preventDefault();
-        const target = $(this).data("target");
-        $(".container-item").hide();
-        $("#" + target).show();
-        $(".subitem-menu").removeClass("selected-item");
-        $(this).addClass("selected-item");
-    });
+
 
     $(".supplier-name").click(function(event) {
         event.preventDefault();
@@ -102,148 +103,5 @@ $(document).ready(function() {
 
 });
 
-function loadSuppliers() {
-    /* $.ajax({
-        url: "/supplier",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            let suppliers = data.suppliers;
-            let html = "";
-            suppliers.forEach(supplier => {
-                html += `<div class="supplier-name" data-phone="${supplier.phone}">${supplier.name}</div>`;
-            });
-            $(".suppliers-list").html(html);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-    */
-
-    let suppliers = [
-        new Supplier("Juan Perez", "12345678", "Banco de Costa Rica", "Cuenta Corriente", "123456789", "Cheque", "Ninguna"),
-        new Supplier("María Rodríguez", "98765432", "Banco Nacional", "Cuenta de Ahorro", "987654321", "Transferencia", "Contrato vigente"),
-        new Supplier("Pedro Gómez", "55555555", "Banco Popular", "Cuenta Corriente", "555555555", "Cheque", "Servicio mensual"),
-        new Supplier("Ana Sánchez", "33333333", "Banco BAC", "Cuenta de Ahorro", "333333333", "Transferencia", "Ninguna"),
-        new Supplier("Carlos López", "77777777", "Banco Davivienda", "Cuenta Corriente", "777777777", "Cheque", "Mantenimiento trimestral"),
-        new Supplier("Luisa Martínez", "99999999", "Banco HSBC", "Cuenta de Ahorro", "999999999", "Transferencia", "Contrato anual"),
-        new Supplier("Miguel González", "11111111", "Banco Scotiabank", "Cuenta Corriente", "111111111", "Cheque", "Servicio mensual"),
-        new Supplier("Elena Morales", "44444444", "Banco Citibank", "Cuenta de Ahorro", "444444444", "Transferencia", "Ninguna")
-    ];
-
-    const tableRow = $("<tr>").addClass("table-row");
-    const supplierName = $("<td>").addClass("supplier-name");
-    const supplierPhone = $("<td>");
-    const supplierBank = $("<td>");
-    const supplierAccount = $("<td>");
-    const supplierAccountNumber = $("<td>");
 
 
-    suppliers.forEach(supplier => {
-        const newRow = tableRow.clone();
-
-        const clonedSupplierName = supplierName.clone();
-        const clonedSupplierPhone = supplierPhone.clone();
-        const clonedSupplierAccountNumber = supplierAccountNumber.clone();
-        const clonedSupplierBank = supplierBank.clone();
-        const clonedSupplierAccount = supplierAccount.clone();
-    
-        clonedSupplierName.text(supplier.name).addClass("supplier-name");
-        clonedSupplierPhone.text(supplier.phone);
-        clonedSupplierAccountNumber.text(supplier.accountNumber);
-        clonedSupplierBank.text(supplier.bank);
-        clonedSupplierAccount.text(supplier.account);
-    
-        newRow.append(clonedSupplierName, clonedSupplierPhone, clonedSupplierAccountNumber, clonedSupplierBank, clonedSupplierAccount);
-    
-        $("#table-suppliers").append(newRow);
-    });
-    
-}
-
-function loadRecordsForPay() {
-    /* $.ajax({
-        url: "/supplier",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            let suppliers = data.suppliers;
-            let html = "";
-            suppliers.forEach(supplier => {
-                html += `<div class="supplier-name" data-phone="${supplier.phone}">${supplier.name}</div>`;
-            });
-            $(".suppliers-list").html(html);
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    }); */
-
-    let recordsForPay = [
-        new RecordForPay("Juan Perez", "12345678", "29-11-23", "Q230.00", true),
-        new RecordForPay("María Rodríguez", "98765432", "29-10-23", "Q2250.00", true),
-        new RecordForPay("Pedro Gómez", "55555555", "29-12-23", "Q1530.00", false),
-    ];
-
-
-    const tableRow = $("<tr>").addClass("table-row");
-    const reocrdSupplier = $("<td>").addClass("record-supplier-name");
-    const recordNumberBill = $("<td>");
-    const recordDate = $("<td>");
-    const recordPrice = $("<td>");
-    const recordState = $("<td>");
-    const recordCheckBox = $("<input>").attr("type", "checkbox");
-
-
-    recordsForPay.forEach(record => {
-        const newRow = tableRow.clone();
-
-        const clonedReocrdSupplier = reocrdSupplier.clone();
-        const clonedRecordNumberBill = recordNumberBill.clone();
-        const clonedRecordDate = recordDate.clone();
-        const clonedRecordPrice = recordPrice.clone();
-        const clonedRecordState = recordState.clone();
-        const clonedRecordCheckBox = recordCheckBox.clone();
-    
-        clonedReocrdSupplier.text(record.supplier);//.addClass("supplier-name");
-        clonedRecordNumberBill.text(record.numberBill);
-        clonedRecordDate.text(record.date);
-        clonedRecordPrice.text(record.amount);
-        
-        if(record.isPayed){
-            //clonedRecordState.text("Pagado");
-            clonedRecordCheckBox.attr("checked", "checked");
-        }else{
-            //clonedRecordState.text("Pendiente");
-            clonedRecordCheckBox.removeAttr("checked");
-        }
-        clonedRecordState.append(clonedRecordCheckBox);
-
-        newRow.append(clonedReocrdSupplier, clonedRecordNumberBill, clonedRecordDate, clonedRecordPrice, clonedRecordState);
-    
-        $("#table-record-for-pay").append(newRow);
-    });
-}
-
-
-class Supplier {
-    constructor(name, phone, bank, account, accountNumber, paymentMethod, description) {
-        this.name = name;
-        this.phone = phone;
-        this.bank = bank;
-        this.account = account;
-        this.accountNumber = accountNumber;
-        this.paymentMethod = paymentMethod;
-        this.description = description;
-    }
-}
-class RecordForPay{
-    constructor(supplier, numberBill ,date, amount, isPayed){
-        this.supplier = supplier;
-        this.numberBill = numberBill;
-        this.date = date;
-        this.amount = amount;
-        this.isPayed = isPayed;
-    }
-}
